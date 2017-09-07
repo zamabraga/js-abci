77e508fb76f2fb9cb683e3f1d90f4c193ceda217
drr00t <adribeiro@gmail.com>
2017-09-06 23:51:53 -0300

    log blockchain workflow

--- example/app.js
+++ example/app.js
@@ -6,7 +6,14 @@ class CounterApp extends abci.ABCIApplication{
     super();
     this.hashCount = 0;
     this.txCount = 0;
-    this.serial = true; 
+    this.serial = true;
+    this.blockStore = {state:[],
+      height:0,
+      last_commit_hash:"",
+      last_block_id:"",
+      last_commit_hash:"",
+      app_hash:""
+    }; 
   }
 
   info(req, cb) {
@@ -18,65 +25,6 @@ class CounterApp extends abci.ABCIApplication{
       last_block_height: self.height,
       last_block_app_hash: self.app_hash
     });
-  }
-  beginBlock(req, cb) {
-    let self = this;
-
-    self.height = req.begin_block.header.height;
-    self.last_block_id = req.begin_block.header.last_block_id.hash;
-    self.last_commit_hash = req.begin_block.header.last_commit_hash;
-    self.data_hash = req.begin_block.header.data_hash;
-    self.validators_hash = req.begin_block.header.validators_hash;
-    self.app_hash = req.begin_block.header.app_hash;
-
-    console.log("\nblock height: %j", self.height);
-    console.log("\nblock last_block_id: %j", self.last_block_id);
-    console.log("\nblock last_commit_hash: %j", self.last_commit_hash);
-    console.log("\nblock data_hash: %j", self.data_hash);
-    console.log("\nblock validators_hash: %j", self.validators_hash);
-    console.log("\nblock app_hash: %j", self.app_hash);
-
-    return cb({});
-  }
-  endBlock(req, cb) {
-    // console.log("\nblock end: %j", req);
-    return cb({});
-  }
-  setOption(req, cb) {
-    console.log("\ndeliver: %j",req.set_option);
-    let self = this;
-    if (req.set_option.key == "serial") {
-      if (req.set_option.value == "on") {
-        self.serial = true;
-        return cb({log:"ok"});
-      } else if (req.set_option.value == "off") {
-        self.serial = false;
-        return cb({log:"ok"});
-      } else {
-        return cb({log:"Unexpected value "+req.set_option.value});
-      }
-    }
-    return cb({log: "Unexpected key "+req.set_option.key});
-  }
-
-  deliverTx(req, cb) {
-    console.log("\ndeliver: %j",req.deliver_tx);
-    
-    let self = this;
-    let txBytes = req.deliver_tx.tx.toBuffer();
-    if (self.serial) {
-      if (txBytes.length >= 2 && txBytes.slice(0, 2) == "0x") {
-        let hexString = txBytes.toString("ascii", 2);
-        let hexBytes = Buffer.from(hexString, "hex");
-        txBytes = hexBytes;
-      }	
-      var txValue = txBytes.readUIntBE(0, txBytes.length);
-      if (txValue != self.txCount){
-        return cb({code:abci.CodeType.BadNonce, log:"Nonce is invalid. Got "+txValue+", expected "+this.txCount});
-      }
-    }
-    self.txCount += 1;
-    return cb({code:abci.CodeType_OK});
   }
 
   checkTx(req, cb) {
@@ -99,9 +47,64 @@ class CounterApp extends abci.ABCIApplication{
     return cb({code:abci.CodeType_OK});
   }
 
+  beginBlock(req, cb) {
+    let self = this;
+
+    self.height = req.begin_block.header.height;
+    self.last_block_id = req.begin_block.header.last_block_id.hash;
+    self.last_commit_hash = req.begin_block.header.last_commit_hash;
+    self.data_hash = req.begin_block.header.data_hash;
+    self.validators_hash = req.begin_block.header.validators_hash;
+    self.app_hash = req.begin_block.header.app_hash;
+
+    self.blockStore.height = self.height;
+    self.blockStore.last_block_id = self.last_block_id.toString("hex");
+    self.blockStore.last_commit_hash = self.last_commit_hash.toString("hex");
+    self.blockStore.app_hash = self.app_hash.toString("hex");
+    // console.log("\nblock height: %s", self.height.toString('hex'));
+    // console.log("\nblock last_block_id: %s", self.last_block_id.hash.toString('hex'));
+    // console.log("\nblock last_commit_hash: %s", self.last_commit_hash.toString('hex'));
+    // console.log("\nblock data_hash: %s", self.data_hash.toString('hex'));
+    // console.log("\nblock validators_hash: %s", self.validators_hash.toString('hex'));
+    // console.log("\nblock app_hash: %s", self.app_hash.toString('hex'));
+
+    return cb({});
+  }
+  endBlock(req, cb) {
+    let self = this;
+    self.blockStore.app_hash = self.app_hash.toString('hex');
+    this.isEndBlock = true;
+    console.log("\nblock end: %j", self.blockStore);
+
+    return cb({});
+  }
+
+  deliverTx(req, cb) {
+    console.log("\ndeliver: %j",req.deliver_tx);
+    
+    let self = this;
+    let txBytes = req.deliver_tx.tx.toBuffer();
+    if (self.serial) {
+      if (txBytes.length >= 2 && txBytes.slice(0, 2) == "0x") {
+        let hexString = txBytes.toString("ascii", 2);
+        let hexBytes = Buffer.from(hexString, "hex");
+        txBytes = hexBytes;
+      }	
+      var txValue = txBytes.readUIntBE(0, txBytes.length);
+      if (txValue != self.txCount){
+        return cb({code:abci.CodeType.BadNonce, log:"Nonce is invalid. Got "+txValue+", expected "+this.txCount});
+      }
+      self.blockStore.state.push(txValue);
+    }
+
+    self.txCount += 1;
+    
+    return cb({code:abci.CodeType_OK});
+  }
+
   commit(req, cb) {
     let self = this;
-    console.log("commit: %j",req.commit);
+    // console.log("commit: %j",req.commit);
 
     self.hashCount += 1;
     if (self.txCount == 0){
@@ -109,7 +112,34 @@ class CounterApp extends abci.ABCIApplication{
     }
     let buf = Buffer.alloc(8);
     buf.writeIntBE(self.txCount, 0, 8);
+
+    console.log("\commit: %j", self.blockStore);
+
+    // console.log("\nblock height: %d", self.height);
+    // console.log("\nblock last_block_id: %s", self.last_block_id.toString('hex'));
+    // console.log("\nblock last_commit_hash: %s", self.last_commit_hash.toString('hex'));
+    // console.log("\nblock data_hash: %s", self.data_hash.toString('hex'));
+    // console.log("\nblock validators_hash: %s", self.validators_hash.toString('hex'));
+    // console.log("\nblock app_hash: %s", self.app_hash.toString('hex'));
+
     return cb({data:buf});
+  }
+
+  setOption(req, cb) {
+    console.log("\ndeliver: %j",req.set_option);
+    let self = this;
+    if (req.set_option.key == "serial") {
+      if (req.set_option.value == "on") {
+        self.serial = true;
+        return cb({log:"ok"});
+      } else if (req.set_option.value == "off") {
+        self.serial = false;
+        return cb({log:"ok"});
+      } else {
+        return cb({log:"Unexpected value "+req.set_option.value});
+      }
+    }
+    return cb({log: "Unexpected key "+req.set_option.key});
   }
 
   query(req, cb) {
